97192449232d1c01224e200cda1b419cd3a9866f
sayyidkhan <sayyidkhan92@hotmail.com>
2025-03-22 21:37:34 +0800

    vercelZ

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'primereact/resources/themes/lara-light-indigo/theme.css' // Theme
 import 'primereact/resources/primereact.min.css'                 // Core
-import 'primeicons/primeicons.css'                               // Icons
+import './styles/icons.css'                                      // Custom icons (replacing primeicons)
 
 import App from './App.tsx'
 
